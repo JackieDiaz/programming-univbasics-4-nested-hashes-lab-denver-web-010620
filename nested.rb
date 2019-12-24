c00ac87b7f34fef6e84f658d86ffe3f530befb97
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-24 01:18:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,24 +35,9 @@ programmer_hash =
           :languages => ["C"]
         }
      }
+    
      programmer_hash[:known_for]
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
 end
 
 def dennis_ritchies_language
