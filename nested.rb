6fe0ad728c00121442dbab5cb211dbf303ae7db9
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-24 01:32:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ programmer_hash =
         }
      }
      
-     :alan_kay[:known_for]
+     programmer_hash[:alan_kay][:known_for]
 end
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
